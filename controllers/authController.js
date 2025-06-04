4b333ac9b2d0e81a8b2efc164632b09506326af8
Farhan Ahmad <bc190409805@vu.edu.pk>
2025-06-04 15:58:39 +0500

    Add Edit-Profile Fully Functional Page

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,18 +173,24 @@ exports.postLogout = (req, res, next) => {
 }
 
 exports.getProfile = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
-    return res.redirect("/login");
-  }
+  if (!req.session.isLoggedIn) return res.redirect("/login");
+
+  const successMessage = req.session.successMessage;
+  const errorMessage = req.session.errorMessage;
+
+  req.session.successMessage = null;
+  req.session.errorMessage = null;
+
   res.render("auth/profile", {
     pageTitle: "Profile",
     currentPage: "profile",
     isLoggedIn: true,
     user: req.session.user,
-    errors: [],
-    oldInput: {},
+    message: successMessage,
+    errorMessage: errorMessage
   });
 };
+
 
 exports.postProfile = async (req, res, next) => {
   if (!req.session.isLoggedIn) {
@@ -203,3 +209,73 @@ exports.postProfile = async (req, res, next) => {
     });
   }
 }
+
+exports.getEditProfile = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+  res.render("auth/editProfile", {
+    pageTitle: "Edit Profile",
+    currentPage: "editProfile",
+    isLoggedIn: true,
+    user: req.session.user,
+    errors: [],
+    oldInput: {
+      firstName: req.session.user.firstName,
+      lastName: req.session.user.lastName,
+      email: req.session.user.email,
+      userType: req.session.user.userType,
+    },
+  });
+}
+
+exports.postEditProfile = async (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+
+  const { firstName, lastName, userType } = req.body;
+
+  try {
+    const user = await User.findById(req.session.user._id);
+    if (!user) {
+      return res.status(422).render("auth/editProfile", {
+        pageTitle: "Edit Profile",
+        currentPage: "editProfile",
+        isLoggedIn: true,
+        user: req.session.user,
+        errors: [{ msg: "User not found" }],
+        oldInput: {
+          firstName,
+          lastName,
+          email: req.session.user.email,
+          userType,
+        },
+      });
+    }
+
+    // ✅ Update allowed fields only
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.userType = userType;
+    await user.save();
+
+    // ✅ Update session as well
+    req.session.user.firstName = firstName;
+    req.session.user.lastName = lastName;
+    req.session.user.userType = userType;
+
+    // ✅ Set only success message
+    req.session.successMessage = "Profile updated successfully!";
+    
+    res.redirect("/profile");
+  } catch (err) {
+    console.error("Edit Profile Error:", err);
+
+    // ✅ Only set error if there's an actual failure
+    req.session.errorMessage = "Something went wrong while updating your profile.";
+    
+    res.redirect("/profile");
+  }
+};
+
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -12,5 +12,7 @@ authRouter.get("/signup", authController.getSignup);
 authRouter.post("/signup", authController.postSignup);
 authRouter.get("/profile", authController.getProfile);
 authRouter.post("/profile", authController.postProfile);
+authRouter.get("/editprofile", authController.getEditProfile);
+authRouter.post("/editprofile", authController.postEditProfile);
 
 module.exports = authRouter;
